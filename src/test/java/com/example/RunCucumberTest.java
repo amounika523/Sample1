83f5e3f11258a95d6871dba9f185075240428003
amounika523 <amounika523@gmail.com>
2025-05-24 20:35:08 -0500

    first

--- src/test/java/com/example/RunCucumberTest.java
+++ src/test/java/com/example/RunCucumberTest.java
@@ -9,7 +9,7 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features/api.feature")
+@SelectClasspathResource("features")
 //@ConfigurationParameter(key = TAGS_PROPERTY_NAME, value = "@smoke")
 @ConfigurationParameter(key = "cucumber.glue", value = "com.example.steps")
 @ConfigurationParameter(key = "cucumber.plugin", value = "pretty, html:target/cucumber-report.html")
